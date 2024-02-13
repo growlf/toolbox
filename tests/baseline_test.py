1cdd64563fa36b63063873eca3e19a832f35f5fe
JaydenMikulcik <jjmikulcik@gmail.com>
2024-02-13 11:26:50 -0700

    Jayden Mikulcik Work?

--- tests/baseline_test.py
+++ tests/baseline_test.py
@@ -1,7 +1,11 @@
 # test_with_pytest.py
+import random
 
 def test_always_passes():
-    assert True
+    if random.randint(1,3) > 2:
+        assert True
+    else:
+        assert False
 
 # def test_always_fails():
 #     assert False
