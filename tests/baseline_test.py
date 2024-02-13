6a2a4bbe17e891764fe85feeea975818d0f4c852
J-Froese <josh.froese@ucalgary.ca>
2024-02-13 11:41:17 -0700

    Tests always fail

--- tests/baseline_test.py
+++ tests/baseline_test.py
@@ -1,7 +1,7 @@
 # test_with_pytest.py
 
-def test_always_passes():
-    assert True
+#def test_always_passes():
+#    assert True
 
-# def test_always_fails():
-#     assert False
+def test_always_fails():
+    assert False
