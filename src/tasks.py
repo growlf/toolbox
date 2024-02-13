3ed7d11d74368e946a2e87c07b22567ab7f02562
EadanL <eadan.lay@ucalgary.ca>
2024-02-13 11:39:52 -0700

    Revert "Added docker_compose_version method"
    This reverts commit a7368265e70f0fa955f76b34c5e0d7820c7cbe35.

--- src/tasks.py
+++ src/tasks.py
@@ -2,17 +2,16 @@ from invoke import task
 import invoke
 import logging
 import docker
-import pyfiglet
+import pyfiglet 
 import speedtest
+
 
 
 ###########################################
 # Library functions
 
-# Create a logger object
+# Create a logger object 
 logger = logging.getLogger(__name__)
-
-
 def _set_log_level(verbose=0) -> None:
     """Set the logging level
 
@@ -20,20 +19,22 @@ def _set_log_level(verbose=0) -> None:
         verbose (int, optional): verbosity level [0-4]. Defaults to 0.
     """
 
-    log_levels = ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"]
+    log_levels = ["CRITICAL", "ERROR",  "WARNING", "INFO", "DEBUG"]
 
     # Ensure verbosity stays within the tollerances
     verbose = 4 if verbose > 4 else verbose
 
     # Actually set the logging level
     logging.basicConfig(level=log_levels[verbose])
-
+    
 
 ###########################################
 # Tasks
 
-
-@task(default=True, incrementable=["verbose"])
+@task(
+    default=True, 
+    incrementable=["verbose"]
+    )
 def help(c, verbose=0):
     """Shows the help screen.
 
@@ -46,25 +47,19 @@ def help(c, verbose=0):
     ascii_banner = pyfiglet.figlet_format("Tool Box")
     print(ascii_banner)
 
-    print(
-        "Try running `inv --help` for more information or `inv --list` to see a list of subcommands. "
-    )
-    print(
-        "Some additional commands that are also installed and might be of use:  whois, speedtest, mtr, tcpdump, nmap, nc"
-    )
-
+    print("Try running `inv --help` for more information or `inv --list` to see a list of subcommands. ")
+    print("Some additional commands that are also installed and might be of use:  whois, speedtest, mtr, tcpdump, nmap, nc")
 
 @task(
-    help={
-        "name": "Name of the person to say hi to.",
-        "verbose": "Logging and verbosity level",
-    },
-    optional=["name"],
-    incrementable=["verbose"],
-)
+    help={'name': "Name of the person to say hi to.",
+        'verbose': "Logging and verbosity level"
+        }, 
+    optional=['name'],
+    incrementable=['verbose']
+    )
 def hello(c, name="world", verbose=0):
     """Say hello
-
+    
     Args:
         c (_type_): _description_
         name (str, optional): _description_. Defaults to "world".
@@ -77,9 +72,10 @@ def hello(c, name="world", verbose=0):
     logger.debug("Said hello.")
 
 
-@task(incrementable=["verbose"])
+@task(incrementable=['verbose'])
 def netspeed(c, verbose=0):
-    """Run an internet speedtest"""
+    """Run an internet speedtest
+    """
 
     _set_log_level(verbose)
 
@@ -100,7 +96,7 @@ def netspeed(c, verbose=0):
     s.upload(threads=threads)
 
     results_dict = s.results.dict()
-
+    
     logger.debug(results_dict)
     logger.debug(s.results.server)
     print(f"{'PING':<20}: {int(s.results.ping)}ms")
@@ -108,23 +104,24 @@ def netspeed(c, verbose=0):
     print(f"{'DOWNLOAD':<20}: {int(s.results.download / 1024 / 1024)}Mbps")
 
 
-@task(incrementable=["verbose"])
+@task(incrementable=['verbose'])
 def dockerinfo(c, verbose=0):
-    """Get Docker installation information"""
+    """Get Docker installation information
+    """
 
     _set_log_level(verbose)
 
     logger.debug("Looking for Docker installation...")
 
     try:
-        client = docker.from_env()
+        client = docker.from_env()        
     except Exception as e:
         raise invoke.Exit("ERROR: Docker connection failed.", e)
     info = client.info()
     plugins = client.plugins.list()
     client.close()
 
-    print("-" * 40)
+    print('-'*40)
     print(f"{'System Name':<20}: {info.get('Name')}")
     print(f"{'Árchetecture':<20}: {info.get('Architecture')}")
     print(f"{'OSType':<20}: {info.get('OSType')}")
@@ -132,66 +129,55 @@ def dockerinfo(c, verbose=0):
     print(f"{'Operating System':<20}: {info.get('OperatingSystem')}")
     print(f"{'System Time':<20}: {info.get('SystemTime')}")
 
-    print("-" * 40)
+    print('-'*40)
     print(f"{'Docker Version':<20}: {info.get('ServerVersion')}")
     print(f"{'Docker Root':<20}: {info.get('DockerRootDir')}")
     print(f"{'Logging Driver':<20}: {info.get('LoggingDriver')}")
     print(f"{'Images':<20}: {info.get('Images')}")
     print(f"{'Containers':<20}: {info.get('Containers')}")
     print(f"{'Containers Running':<20}: {info.get('ContainersRunning')}")
-    loki = next((x for x in plugins if x.name == "loki:latest"), None)
+    loki = next((x for x in plugins if x.name == 'loki:latest'), None)
     if loki:
         print(f"{'Loki':<20}: {loki.name}")
         print(f"{' ':<20}: {'enabled' if loki.enabled else 'disabled'}")
         for setting in loki.settings:
             print(f"{' ':<20}: {setting}:{loki.settings.get(setting)}")
     else:
-        print(f"{'Loki':<20}: {'Loki not found'}")
+        print(f"{'Loki':<20}: {'Loki not found'}")        
 
-    print("-" * 40)
+    print('-'*40)
     print(f"{'HttpProxy':<20}: {info.get('HttpProxy')}")
     print(f"{'HttpsProxy':<20}: {info.get('HttpsProxy')}")
     print(f"{'NoProxy':<20}: {info.get('NoProxy')}")
-    print("-" * 40)
+    print('-'*40)
 
-    # from pprint import pprint
-    # pprint(ddf['Volumes'])
+    #from pprint import pprint
+    #pprint(ddf['Volumes'])
 
-    ####TODO: check outbound connections
-    print(f"{'verify correct time':<20}: {'TBD'}")
-    ####TODO: add task to check remaining disk size on host system
-    print(f"{'Drive space':<20}: {'TBD'}")
-    ####TODO: check outbound connections
-    print(f"{'Firewall':<20}: {'TBD'}")
-    # declare -a urls=("https://www.google.com 200"
+####TODO: check outbound connections
+    print(f"{'verify correct time':<20}: {'TBD'}")        
+####TODO: add task to check remaining disk size on host system
+    print(f"{'Drive space':<20}: {'TBD'}")        
+####TODO: check outbound connections
+    print(f"{'Firewall':<20}: {'TBD'}")        
+    #declare -a urls=("https://www.google.com 200"
     # "https://arch.archfx.io/api/v1/server/ 200"
     # "https://portainer.overseer.archfx.io 200"
     # "https://portaineredge.overseer.archfx.io 404"
     # "https://ecr.archfx.io 401"
-
-
 ####TODO: get docker-compose version
-@task(incrementable=["verbose"])
-def docker_compose_version(c, verbose=0):
-    try:
-        client = docker.from_env()
-    except Exception as e:
-        raise invoke.Exit("ERROR: Docker connection failed.", e)
-    info = client.info()
-
-    print("Docker-compose version: ", info.get("ServerVersion"))
-
-
-@task(incrementable=["verbose"])
+    
+@task(incrementable=['verbose'])
 def dockertest(c, verbose=0):
-    """Test Docker installation"""
+    """Test Docker installation
+    """
 
     _set_log_level(verbose)
 
     logger.debug("Testing Docker installation...")
 
     try:
-        client = docker.from_env()
+        client = docker.from_env()        
     except Exception as e:
         raise invoke.Exit("ERROR: Docker connection failed.", e)
     info = client.info()
@@ -200,7 +186,6 @@ def dockertest(c, verbose=0):
     client.images.pull("netyeti/toolbox:latest")
 
     from pprint import pprint
-
     pprint(client.swarm.attrs)
 
     client.close()
