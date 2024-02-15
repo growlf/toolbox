eed2a5f651f193861abdaed27bcd88cecd9479c4
Dominic Gartner <dominic.gartner@ucalgary.ca>
2024-02-15 12:12:14 -0700

    added test for proxy in evironment

--- tests/baseline_test.py
+++ tests/baseline_test.py
@@ -1,7 +1,11 @@
 # test_with_pytest.py
-
+import os
 def test_always_passes():
     assert True
 
 # def test_always_fails():
 #     assert False
+
+def test_proxy_environment_variables():
+    assert 'http_proxy' in os.environ
+    assert 'https_proxy' in os.environ
